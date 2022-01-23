8464b753df0c2ae38c7113ae4b2510110b167630
bt21104023-NiteshBhardwaj <97734526+bt21104023-NiteshBhardwaj@users.noreply.github.com>
2022-01-23 19:37:03 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: D:\NITESH FILES\PROJECTS\Pyhton projects\Combined Assignment.py ===
+
+
+
+
+WELCOME
+Enter first number:10
+Enter second number:20
+Enter third number:30
+The average of the entered three number is 20.0
+
+
+
+
+Let's calculate your income tax 
+if u have income less than 30000 u will not have to pay any tax
+
+What is your income : 50000
+You will have a standard deduction of 10000$
+How many dependent do u have :5
+You will have a deduction of 3000$ per dependent 
+Your taxable income is 25000.0$
+Hence u will have to pay 5000.0$ as tax
+
+
+
+
+Enter the following values to obtain a list
+Enter your name: Nitesh Bhardwaj
+Enter your SID: 21104023
+Enter your gender ('M' 'F' 'U'): M
+Enter your course name: Electrical
+Enter your CGPA: 10
+[21104023, 'Nitesh Bhardwaj', 'M', 'Electrical', 10.0]
+
+
+
+
+Enter marks of 1st student :5
+Enter marks of 2nd student :9
+Enter marks of 3rd student :8
+Enter marks of 4th student :10
+Enter marks of 5th student :2
+normal list [5, 9, 8, 10, 2]
+sorted list [2, 5, 8, 9, 10]
+
+
+
+
+normal output =['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+output of part a = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+output of part b = ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
+
+
+
+
